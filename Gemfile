4c4f2e15b65ea7ca49d1c4a70c21588b565afd6a
Zabbkagit <gryzlee@gmail.com>
2015-02-16 23:35:23 +0100

    set devise admin panel

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Authorization & Authentication
 
 gem 'devise'
-
+gem 'cancancan', '~> 1.10'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-	def configure_permitted_parameters
-	  devise_parameter_sanitizer.for(:sign_up) << :name
-	  devise_parameter_sanitizer.for(:account_update) << :name
-	end
+	protected
+		def configure_permitted_parameters
+		  devise_parameter_sanitizer.for(:sign_up) << :name
+		  devise_parameter_sanitizer.for(:account_update) << :name
+		end
 end
